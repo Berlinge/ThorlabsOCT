acc097b56b55111f8b615444e2d8d60ef230b2b9
Berlinge <68648056+Berlinge@users.noreply.github.com>
2022-03-08 18:58:12 +0100

    Delete functions_dissertation directory

--- functions_dissertation/loadOffline.m
+++
@@ -1,8 +0,0 @@
-    % Easy offline loader - no need for main_gui.m
-% Input: inf.mat file of the dataset
-% path = 'D:\2019-12-19\';
-% file = '\20191219_Zunge_0028_Mode2D_inf.mat';
-% close all;
-clearvars;
-[file,path] = uigetfile;
-[Raw,inf,~] = load_to_memory(path,file,dbstack);
